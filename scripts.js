0ed61d05151d62916740fe740d30b5c967bdf805
kxnnxthmxlgxs <73911764+kxnnxthmxlgxs@users.noreply.github.com>
2024-12-30 11:52:11 +0200

    Made changes to portfolio section, changed from slider to listed view, because in mobile view it did not work correctly

--- scripts.js
+++ scripts.js
@@ -1,34 +1,6 @@
 //portfolio card slider javascript
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Initialize Swiper
-    const swiper = new Swiper('.swiper-container', {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-            },
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-            },
-            600: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-            }
-        }
-    });
-});
+
 
 
 
